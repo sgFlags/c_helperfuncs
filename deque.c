3ffa3e078dde6e06b6afc403ce7b37bfbadb61e2
sgFlags <bwhcolin@outlook.com>
2019-10-13 11:46:19 +0800

    Update deque.c

--- deque.c
+++ deque.c
@@ -22,12 +22,12 @@ Deque *dequeCreate(int size)
 	return deque;
 }
 
-inline int _formalize(int number, int max)
+static inline int _formalize(int number, int max)
 {
 	return (number + max) % max;
 }
 
-inline void _dequeRealloc(Deque *deque)
+static inline void _dequeRealloc(Deque *deque)
 {
 	DequeType *temp = NULL;
 	int firstHalf = deque->head;
@@ -42,7 +42,7 @@ inline void _dequeRealloc(Deque *deque)
 	deque->tail = deque->size - 1;
 }
 
-inline void dequeAddHead(Deque *deque, DequeType value)
+static inline void dequeAddHead(Deque *deque, DequeType value)
 {
 	if (deque->size >= deque->maxSize) {
 		_dequeRealloc(deque);
@@ -52,7 +52,7 @@ inline void dequeAddHead(Deque *deque, DequeType value)
 	deque->size++;
 }
 
-inline void dequeAddTail(Deque *deque, DequeType value)
+static inline void dequeAddTail(Deque *deque, DequeType value)
 {
 	if (deque->size >= deque->maxSize) {
 		_dequeRealloc(deque);
@@ -62,34 +62,34 @@ inline void dequeAddTail(Deque *deque, DequeType value)
 	deque->size++;
 }
 
-inline DequeType dequePeekHead(Deque *deque)
+static inline DequeType dequePeekHead(Deque *deque)
 {
 	return deque->values[deque->head];
 }
 
-inline DequeType dequePeekTail(Deque *deque)
+static inline DequeType dequePeekTail(Deque *deque)
 {
 	return deque->values[deque->tail];
 }
 
-inline void dequePopHead(Deque *deque)
+static inline void dequePopHead(Deque *deque)
 {
 	deque->head = _formalize(deque->head + 1, deque->maxSize);
 	deque->size--;
 }
 
-inline void dequePopTail(Deque *deque)
+static inline void dequePopTail(Deque *deque)
 {
 	deque->tail = _formalize(deque->tail - 1, deque->maxSize);
 	deque->size--;
 }
 
-inline DequeType dequeGet(Deque *deque, int index)
+static inline DequeType dequeGet(Deque *deque, int index)
 {
 	return deque->values[(deque->head + index) % deque->maxSize];
 }
 
-inline int getSize(Deque *deque)
+static inline int getSize(Deque *deque)
 {
 	return deque->size;
 }
